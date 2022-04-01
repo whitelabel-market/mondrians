a368a56a30fa9595fa8602ad29960f9ca0ddcff0
Fabian Piper <fabianpiper@web.de>
2022-04-01 23:17:59 +0200

    Init tailwind + font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
-import { defineNuxtConfig } from 'nuxt3'
+import {defineNuxtConfig} from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+    meta: {
+        link: [
+            {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
+            {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossOrigin: true},
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css2?family=Birthstone+Bounce&family=Inter:wght@400;500;600;700&display=swap'
+            }
+        ]
+    },
+    buildModules: ['@nuxtjs/tailwindcss'],
 })
