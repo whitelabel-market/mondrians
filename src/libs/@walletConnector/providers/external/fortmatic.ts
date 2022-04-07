3541e13fe48161026428936733b95edc8f93a2f5
Fabian Piper <fabianpiper@web.de>
2022-04-07 19:17:03 +0200

    Add wallet connector lib

---
+++ src/libs/@walletConnector/providers/external/fortmatic.ts
@@ -0,0 +1,33 @@
+// @ts-ignore
+import Fortmatic from "fortmatic";
+import {
+  ProviderType,
+  type ConnectorOptions,
+  type IProvider,
+} from "../../types";
+import { FortmaticLogo } from "../logos/";
+
+const ConnectToFortmatic = async (options: ConnectorOptions) => {
+  const key = options.fortmatic.key;
+  const network = options.networkName;
+  const fm = new Fortmatic(key, network);
+  const provider = await fm.getProvider();
+  // provider.fm = fm;
+  await fm.user.login();
+  const isLoggedIn = await fm.user.isLoggedIn();
+  if (isLoggedIn) {
+    return provider;
+  } else {
+    throw new Error("Failed to login to Fortmatic");
+  }
+};
+
+const FORTMATIC: IProvider = {
+  id: "fortmatic",
+  name: "Fortmatic",
+  logo: FortmaticLogo,
+  type: ProviderType.WEB,
+  connect: ConnectToFortmatic,
+};
+
+export default FORTMATIC;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,12 +6,23 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import Connector from "@/libs/@walletConnector";
 
 export default defineComponent({
   name: "HomeView",
   components: {
     HelloWorld,
+  },
+  setup() {
+    const providers = Connector.init({
+      appName: "Mondrians",
+      infuraId: "",
+      authereum: { key: "" },
+      fortmatic: { key: "" },
+    }).providers;
+
+    console.log("providers", providers);
   },
 });
 </script>
