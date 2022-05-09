60091c8f4bd60da987e1b4502b40d30ade65892b
Fabian Piper <fabianpiper@web.de>
2022-05-09 12:58:05 +0200

    Refactor button styling computation

--- src/components/app/AppButton.vue
+++ src/components/app/AppButton.vue
@@ -1,25 +1,17 @@
 <template>
   <component
-    :is="is && is"
+    :is="is"
     :hover="hover"
-    :type="!to && type"
-    class="relative block cursor-pointer group transition duration-200 ease-out-circ transform translate-x-1 translate-y-1"
-    :to="to && to"
-    :href="href && href"
+    :type="!isRouterOrAnchorLink && type"
+    :to="isRouterLink && to"
+    :href="isAnchorLink && href"
     :disabled="disabled"
     @mouseenter="hover = true"
     @mouseleave="hover = false"
     @click.prevent="!(loading || disabled) && $emit('clicked', $event)"
-    :class="[!disabled && 'active:scale-[0.98]', fullWidth && 'w-full']"
+    :class="classesWrapper"
   >
-    <span
-      class="flex items-center border font-semibold uppercase transform transition duration-200 ease-out-circ group-hover:translate-x-0 group-hover:translate-y-0"
-      :class="[
-        ...classes,
-        center && 'justify-center',
-        !disabled && '-translate-x-1 -translate-y-1',
-      ]"
-    >
+    <span :class="classesContent">
       <AppLoadingSpinner
         v-if="loading"
         :size="'xs'"
@@ -30,7 +22,7 @@
     </span>
     <span
       class="block absolute bottom-0 right-0 block -z-10 !bg-white !w-full !h-full border"
-      :class="[...classes]"
+      :class="[...classesPseudo]"
     ></span>
   </component>
 </template>
@@ -40,41 +32,6 @@ import { computed, ref } from "vue";
 import AppLoadingSpinner from "@/components/app/AppLoadingSpinner.vue";
 
 defineEmits(["clicked"]);
-const hover = ref(false);
-
-const ButtonColor: { [key: string]: string } = {
-  link: "bg-transparent text-current border-transparent",
-  primary: "bg-yellowish text-black border-black",
-  secondary: "bg-gray-100 text-gray-500 border-black",
-  reddish: "bg-reddish text-white border-black",
-  disabled: "cursor-default border-black",
-};
-
-const ButtonSize: (
-  icon: boolean,
-  dense: boolean
-) => { [key: string]: string } = (icon, dense) => {
-  if (icon) {
-    return {
-      xs: "w-6 h-6 text-xs",
-      sm: "w-10 h-10 text-xs",
-      md: "w-12 h-12 text-xs",
-      lg: "w-14 h-14 text-sm",
-    };
-  } else {
-    return {
-      xs: dense ? "p-1 space-x-1 text-xs" : "h-8 py-2 px-4 space-x-1 text-xs",
-      sm: dense ? "p-2 space-x-1 text-xs" : "h-10 py-4 px-6 space-x-2 text-xs",
-      md: dense
-        ? "py-2 px-4 space-x-2 text-sm"
-        : "h-12 py-4 px-8 space-x-2 text-sm",
-      lg: dense
-        ? "py-4 px-8 space-x-2 text-base"
-        : "h-14 py-6 px-14 space-x-2 text-base",
-    };
-  }
-};
-
 const props = defineProps({
   type: {
     type: String,
@@ -110,10 +67,6 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
-  dense: {
-    type: Boolean,
-    default: false,
-  },
   center: {
     type: Boolean,
     default: true,
@@ -138,23 +91,69 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
-  as: {
-    type: String as () => "button" | "a" | "router-link",
-    default: "button",
-  },
 });
 
-const classes = computed(() =>
-  props.color === "custom"
-    ? []
-    : [
-        !props.disabled ? ButtonColor[props.color] : ButtonColor["disabled"],
-        ButtonSize(props.onlyIcon, props.dense)[props.size],
-        props.fullWidth && "w-full",
-        props.loading && "active:scale-100 cursor-default",
-        props.rounded && `rounded-${props.rounded}`,
-      ]
-);
+const hover = ref(false);
 
-const is = !props.to ? (!props.href ? "button" : "a") : "router-link";
+const ButtonColor: { [key: string]: string } = {
+  blank: "bg-transparent text-current border-transparent",
+  link: "bg-transparent text-current border-transparent",
+  primary: "bg-yellowish text-black border-black",
+  secondary: "bg-gray-100 text-gray-500 border-black",
+  reddish: "bg-reddish text-white border-black",
+  disabled: "border-black",
+};
+
+const ButtonWrapperSizeIcon = {
+  xs: "w-6 h-6 text-xs",
+  sm: "w-10 h-10 text-xs",
+  md: "w-12 h-12 text-sm",
+  lg: "w-14 h-14 text-base",
+};
+
+const ButtonWrapperSizeDefault = {
+  xs: "h-8 text-xs",
+  sm: "h-10 text-xs",
+  md: "h-12 text-sm",
+  lg: "h-14 text-base",
+};
+
+const ButtonSpacing = {
+  xs: "py-2 px-4 space-x-1",
+  sm: "py-4 px-6 space-x-2",
+  md: "py-4 px-8 space-x-2",
+  lg: "py-6 px-14 space-x-2",
+};
+
+const classesWrapper = computed(() => [
+  `relative flex items-stretch justify-stretch transition duration-200 ease-out-circ`,
+  props.disabled && props.loading
+    ? "cursor-default"
+    : "cursor-pointer group transform translate-x-1 translate-y-1 active:scale-[0.98]",
+  props.onlyIcon
+    ? ButtonWrapperSizeIcon[props.size]
+    : ButtonWrapperSizeDefault[props.size],
+  props.fullWidth && "w-full",
+  props.rounded && `rounded-${props.rounded}`,
+]);
+
+const classesContent = computed(() => [
+  `flex w-full items-center border font-semibold uppercase transform group-hover:translate-x-0 group-hover:translate-y-0 transition duration-200 ease-out-circ`,
+  ButtonColor[props.disabled ? "disabled" : props.color],
+  !props.onlyIcon && ButtonSpacing[props.size],
+  !props.disabled && "-translate-x-1 -translate-y-1",
+  props.center || props.onlyIcon ? "justify-center" : "justify-between",
+  props.rounded && `rounded-${props.rounded}`,
+]);
+
+const classesPseudo = computed(() => [
+  "block absolute bottom-0 right-0 block bg-white w-full h-full border border-black -z-10",
+  props.rounded && `rounded-${props.rounded}`,
+]);
+
+const isRouterLink = !!props.to;
+const isAnchorLink = !!props.href;
+const isRouterOrAnchorLink = isRouterLink || isAnchorLink;
+
+const is = (isAnchorLink && "a") || (isRouterLink && "router-link") || "button";
 </script>
