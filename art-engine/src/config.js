5f694c0b01ff58399812c84f2c7dfaebbee3b4ee
Kevin Hertwig <kevin.hertwig@gmail.com>
2022-08-19 13:25:05 +0200

    chore: adding art-engine again

---
+++ art-engine/src/config.js
@@ -0,0 +1,133 @@
+"use strict";
+
+const path = require("path");
+const isLocal = typeof process.pkg === "undefined";
+const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
+const { MODE } = require(path.join(basePath, "constants/blend_mode.js"));
+const { NETWORK } = require(path.join(basePath, "constants/network.js"));
+
+const network = NETWORK.eth;
+
+//Image format - select the input format for the layers,  supported values "png", "svg"
+const IMG_FORMAT = "svg";
+
+// General metadata for Ethereum
+const namePrefix = "MAMO";
+const description = "Remember to replace this description";
+const baseUri = "ipfs://NewUriToReplace";
+
+const solanaMetadata = {
+  symbol: "YC",
+  seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
+  external_url: "https://www.youtube.com/c/hashlipsnft",
+  creators: [
+    {
+      address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
+      share: 100,
+    },
+  ],
+};
+
+// If you have selected Solana then the collection starts from 0 automatically
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 1000,
+
+    layersOrder: [
+      { name: "Background" },
+      { name: "Line" },
+      { name: "1", options: {valueAsType: true} },
+      { name: "2", options: {valueAsType: true} },
+      { name: "3", options: {valueAsType: true} },
+      { name: "4", options: {valueAsType: true} },
+      { name: "5", options: {valueAsType: true} },
+      { name: "6", options: {valueAsType: true} },
+      { name: "7", options: {valueAsType: true} },
+      { name: "8", options: {valueAsType: true} },
+    ],
+  },
+];
+
+const shuffleLayerConfigurations = false;
+
+const debugLogs = false;
+
+const format = {
+  width: 512,
+  height: 512,
+};
+
+const gif = {
+  export: false,
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+};
+
+const text = {
+  only: false,
+  color: "#000000",
+  size: 20,
+  xGap: 40,
+  yGap: 40,
+  align: "left",
+  baseline: "top",
+  weight: "regular",
+  family: "Courier",
+  spacer: " => ",
+};
+
+const pixelFormat = {
+  ratio: 2 / 128,
+};
+
+const background = {
+  generate: true,
+  brightness: "0%",
+  static: false,
+  default: "#ffffff",
+};
+
+const extraMetadata = {};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.height / format.width,
+  imageName: "preview.png",
+};
+
+const preview_gif = {
+  numberOfImages: 5,
+  order: "ASC", // ASC, DESC, MIXED
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+  imageName: "preview.gif",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+  pixelFormat,
+  text,
+  namePrefix,
+  network,
+  solanaMetadata,
+  gif,
+  preview_gif,
+  IMG_FORMAT,
+};
